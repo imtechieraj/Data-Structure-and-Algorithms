b88a9fa40d2fc5ada108cbf8042b343dcd09e0a4
Raj kumar <hithisisrajkuar@gmail.com>
2021-11-14 20:01:43 +0530

    Palindromes Solution-2

--- Palindromes/index.js
+++ Palindromes/index.js
@@ -5,5 +5,15 @@ function palindromes(str) {
     let reverseStr = str.split("").reverse().join('');
     return reverseStr === str;
 }
-console.log(palindromes('tata'))
+console.log(palindromes('kakak'))
 //-----------Solution 1 - End-------------//
+
+
+//-----------Solution 2 - Start-------------//
+function palindromes2(str) {
+    return str.split('').every((_item, i) => {
+        return _item === str[str.length - i - 1]
+    });
+}
+console.log(palindromes2('kakak'))
+//-----------Solution 2 - End-------------//
