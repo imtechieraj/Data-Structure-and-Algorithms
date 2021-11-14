592c7d6a1f8e165e5f5f9fac12e540ea1f923789
Raj kumar <hithisisrajkuar@gmail.com>
2021-11-14 19:49:19 +0530

    Reverse STR - solution-2

--- ReverseString/index.js
+++ ReverseString/index.js
@@ -2,11 +2,23 @@
    Output: elppa
 */
 
-function reverseString(str){
-    let str_arr=str.split("");
-    let reverse=str_arr.reverse();
-    let reverse_str=reverse.join('');
+//Solution-1
+function reverseString(str) {
+    let str_arr = str.split("");
+    let reverse = str_arr.reverse();
+    let reverse_str = reverse.join('');
     console.log(reverse_str)
 }
 
+//Solution-2
+
+function reverseString2(str) {
+    let reverse = '';
+    for (let character of str) {
+        reverse = character + reverse;
+    }
+    console.log(reverse)
+}
+
 reverseString("apple");
+reverseString2("apple");
