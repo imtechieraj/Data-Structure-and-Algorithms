6cd596512cd630d3805ba015494224204e5ea95d
Raj kumar <hithisisrajkuar@gmail.com>
2022-01-19 20:11:50 +0530

    Best time to Buy and sell stock

--- Best Time to Buy and Sell Stock/index.js
+++ Best Time to Buy and Sell Stock/index.js
@@ -14,29 +14,11 @@ Find and return the maximum profit you can achieve.
 */
 
 var maxProfit = function (prices) {
-    // let buy=prices[0];
-    // prices[0]=0;
-    // let profit=0;
-
-    // for(let i=1;i<prices.length;i++){
-    //     console.log(prices[i])
-    // if(buy>prices[i]){
-    //     buy=prices[i];
-    //      prices[i]=0;
-    // }else{
-    //     profit=Math.max(prices[i]-buy,profit)
-    // }
-    // }
-    // return profit
-    // let profit = 0;
-    // prices.reduce((preValue, data) => {
-    //     if (data > preValue) {
-    //         currentProfit = data - preValue;
-    //         profit = profit + currentProfit;
-    //         return data
-    //     } return data
-    // }, 0);
-    // return profit;
+    return prices.reduce((prev_value,_item,index)=>{
+        if(_item<prices[index+1]){
+            prev_value=prev_value+prices[index+1]-_item
+        } return prev_value
+    },0)
 };
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]))
